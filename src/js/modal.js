007b9ae949e9d1e6af23c7c782dd80d5b4f8ecd1
Your Name <kirillovaaru@yandex.ru>
2019-09-07 17:17:22 +0300

    Adaptation for smartphones

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,45 +1,16 @@
 $(document).ready(function () {
-    var buttonNav = $('#button-navbar');
-    var buttonDiscount = $('#button-discount');
-    var buttonOffer = $('#button-offer');
-    var modal = $('#modal');
-    var close = $('#close');
+    
+    $('.btn-popup').on('click', function () {
+            $('.popup-form').addClass('modal_active');
+        });
 
-    buttonNav.on('click', function () {
-        modal.addClass('modal_active');
-    });
-
-    buttonDiscount.on('click', function () {
-        modal.addClass('modal_active');
-    });
-
-    close.on('click', function () {
-        modal.removeClass('modal_active');
-    });
-
-});
-
-// всплывающее мини окно
-$(document).ready(function () {
-    var button = $('#buttonmini');
-    var buttons = $('#buttonsmini');
-    var modal = $('#modalmini');
-    var close = $('#closemini');
-
-    button.on('click', function () {
-        modal.addClass('modalmini_active');
-    });
-
-    buttons.on('click', function () {
-        modal.addClass('modalmini_active');
-    });
-
-    close.on('click', function () {
-        modal.removeClass('modalmini_active');
+    $('#close').on('click', function () {
+        $('.popup-form').removeClass('modal_active');
     });
 
     $('#closemini').on('click', function () {
-        $('#modalmini').addClass('class_close');
-    });
+            $('#modalmini').removeClass('modalmini_active');
+        });
+        
 });
 
--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -11,16 +11,16 @@ $mail->CharSet = 'utf-8';
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
+$mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'почта с которой будет отправка';                // Наш логин                 
-$mail->Password = 'тут пароль от почты с которой будет отправка';                           // Наш пароль от ящика
+$mail->Username = 'sender-gora@yandex.ru';                // Наш логин                 
+$mail->Password = 'q1w2e3R$T%Y^';                           // Наш пароль от ящика
                            
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('почта с которой будет отправка', 'Заявка с сайта бла бла бла');   // От кого письмо 
-$mail->addAddress('почта заказчика');     // Add a recipient
+$mail->setFrom('sender-gora@yandex.ru', 'Заявка с Bear mount');   // От кого письмо 
+$mail->addAddress('mail@medvezhiagora.ru');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
@@ -29,7 +29,7 @@ $mail->addAddress('почта заказчика');     // Add a recipient
 //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Заявка с сайта Лендинг Медвежья гора';
+$mail->Subject = 'Заявка с Лендинга Медвежья гора';
 $mail->Body    = '
 	Клиент оставил свои контактные данные<br>
 	Имя клиента : <strong> ' . $name . ' </strong><br>
